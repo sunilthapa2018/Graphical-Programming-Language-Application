9d2d1b2e575e81d90d378385b6383c39aa9c7af1
Sunil Thapa <luck_sunilthapa@yahoo.com>
2019-01-12 16:19:29 +0545

    Drawing with command line
    Drawing with command line

--- Paint/Paint.cs
+++ Paint/Paint.cs
@@ -243,8 +243,8 @@ namespace Paint
                 using (StreamWriter sw = new StreamWriter(s)) {
                     sw.Write(txtCommand.Text);
                 }
-            }
-            MessageBox.Show("Your File has been saved Sucessfully");
+                MessageBox.Show("Your File has been saved Sucessfully");
+            }            
         }
 
         private void btnRun_Click(object sender, EventArgs e)
@@ -254,7 +254,7 @@ namespace Paint
                 if (!validate.isSomethingInvalid)
                 {
                     MessageBox.Show("Everything is working fine");
-                    
+                    RunCommand();
                 }
 
             }
@@ -276,6 +276,216 @@ namespace Paint
                     g.FillRectangle(myBrush, e.X, e.Y, 24, 24);
                 }                
             }
+
+
+        }
+        private void RunCommand() {
+            int numberOfLines = txtCommand.Lines.Length;
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                String oneLineCommand = txtCommand.Lines[i];
+                oneLineCommand = oneLineCommand.Trim();
+                if (!oneLineCommand.Equals(""))
+                {
+                    Boolean hasPlus = oneLineCommand.Contains('+');
+                    Boolean hasEquals = oneLineCommand.Contains("=");
+                    if (hasEquals) {
+                        string[] words2 = oneLineCommand.Split('=');
+                        for (int j = 0; j < words2.Length; j++)
+                        {
+                            words2[j] = words2[j].Trim();
+                        }
+                        if (words2[0].ToLower().Equals("radius")) {
+                            raduis = int.Parse(words2[1]);
+                        } else if (words2[0].ToLower().Equals("width")) {
+                            width = int.Parse(words2[1]);
+                        } else if (words2[0].ToLower().Equals("height")) {
+                            height = int.Parse(words2[1]);
+                        }
+                    } else if (hasPlus) {
+                        string[] words2 = oneLineCommand.Split('+');
+                        for (int j = 0; j < words2.Length; j++)
+                        {
+                            words2[j] = words2[j].Trim();
+                        }
+                        if (words2[0].ToLower().Equals("radius"))
+                        {
+                            raduis += int.Parse(words2[1]);
+                        }
+                        else if (words2[0].ToLower().Equals("width"))
+                        {
+                            width += int.Parse(words2[1]);
+                        }
+                        else if (words2[0].ToLower().Equals("height"))
+                        {
+                            height += int.Parse(words2[1]);
+                        }
+                    }
+                    else {
+                        sendDrawCommand(oneLineCommand);
+                    }
+                    
+                }
+
+            }
+        }
+        private void sendDrawCommand(string lineOfCommand)
+        {
+            String[] shapes = { "circle", "rectangle", "triangle", "polygon" };
+            String[] variable = { "radius", "width", "height", "counter" };
+
+            lineOfCommand = System.Text.RegularExpressions.Regex.Replace(lineOfCommand, @"\s+", " ");
+            string[] words = lineOfCommand.Split(' ');
+            //removing white spaces in between words
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].Trim();
+            }
+            String firstWord = words[0].ToLower();
+            Boolean firstWordShape = shapes.Contains(firstWord);
+            if (firstWordShape) {
+                
+                if (firstWord.Equals("circle")) {
+                    Boolean secondWordIsVariable = variable.Contains(words[1].ToLower());
+                    if (secondWordIsVariable) {
+                        if (words[1].ToLower().Equals("radius")) {
+                            DrawCircle(raduis);
+                        }
+                    }
+                    else {
+                        DrawCircle(Int32.Parse(words[1]));
+                    }
+                    
+                } else if (firstWord.Equals("rectangle")) {
+                    String args = lineOfCommand.Substring(9, (lineOfCommand.Length - 9));
+                    String[] parms = args.Split(',');
+                    for (int i = 0; i < parms.Length; i++)
+                    {
+                        parms[i] = parms[i].Trim();
+                    }
+                    Boolean secondWordIsVariable = variable.Contains(parms[0].ToLower());
+                    Boolean thirdWordIsVariable = variable.Contains(parms[1].ToLower());
+                    if (secondWordIsVariable)
+                    {
+                        if (thirdWordIsVariable)
+                        {
+                            DrawRectangle(width, height);
+                        }
+                        else {
+                            DrawRectangle(width, Int32.Parse(parms[1]));
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (thirdWordIsVariable) {
+                            DrawRectangle(Int32.Parse(parms[0]), height);
+                        }
+                    }                    
+                }
+                else if (firstWord.Equals("triangle"))
+                {
+                    String args = lineOfCommand.Substring(8, (lineOfCommand.Length - 8));
+                    String[] parms = args.Split(',');
+                    for (int i = 0; i < parms.Length; i++)
+                    {
+                        parms[i] = parms[i].Trim();
+                    }
+                    DrawTriangle(Int32.Parse(parms[0]), Int32.Parse(parms[1]), Int32.Parse(parms[2]));
+                }
+                else if (firstWord.Equals("polygon"))
+                {
+                    String args = lineOfCommand.Substring(8, (lineOfCommand.Length - 8));
+                    String[] parms = args.Split(',');
+                    for (int i = 0; i < parms.Length; i++)
+                    {
+                        parms[i] = parms[i].Trim();
+                    }
+                    if (parms.Length == 8){
+                        DrawPolygon(Int32.Parse(parms[0]), Int32.Parse(parms[1]), Int32.Parse(parms[2]), Int32.Parse(parms[3]),
+                            Int32.Parse(parms[4]), Int32.Parse(parms[5]), Int32.Parse(parms[6]), Int32.Parse(parms[7]));
+                    } else if (parms.Length == 10) {
+                        DrawPolygon(Int32.Parse(parms[0]), Int32.Parse(parms[1]), Int32.Parse(parms[2]), Int32.Parse(parms[3]),
+                            Int32.Parse(parms[4]), Int32.Parse(parms[5]), Int32.Parse(parms[6]), Int32.Parse(parms[7]),
+                            Int32.Parse(parms[8]), Int32.Parse(parms[9]));
+                    }
+                    
+                }
+            }             
         }        
+        private void DrawPolygon(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
+        {
+            Pen myPen = new Pen(mainColor);
+            Point[] pnt = new Point[5];
+
+            pnt[0].X = mouseX;
+            pnt[0].Y = mouseY;
+
+            pnt[1].X = mouseX - v1;
+            pnt[1].Y = mouseY - v2;
+
+            pnt[2].X = mouseX - v3;
+            pnt[2].Y = mouseY - v4;
+
+            pnt[3].X = mouseX - v5;
+            pnt[3].Y = mouseY - v6;
+
+            pnt[4].X = mouseX - v7;
+            pnt[4].Y = mouseY - v8;
+
+            g.DrawPolygon(myPen, pnt);
+        }
+        private void DrawPolygon(int v1, int v2, int v3, int v4, int v5,int v6, int v7, int v8, int v9, int v10)
+        {
+            Pen myPen = new Pen(mainColor);
+            Point[] pnt = new Point[6];
+
+            pnt[0].X = mouseX;
+            pnt[0].Y = mouseY;
+
+            pnt[1].X = mouseX - v1;
+            pnt[1].Y = mouseY - v2;
+
+            pnt[2].X = mouseX - v3;
+            pnt[2].Y = mouseY - v4;
+
+            pnt[3].X = mouseX - v5;
+            pnt[3].Y = mouseY - v6;
+
+            pnt[4].X = mouseX - v7;
+            pnt[4].Y = mouseY - v8;
+
+            pnt[5].X = mouseX - v9;
+            pnt[5].Y = mouseY - v10;
+            g.DrawPolygon(myPen, pnt);            
+        }
+
+        private void DrawTriangle(int rBase, int adj, int hyp)
+        {
+            Pen myPen = new Pen(mainColor);
+            Point[] pnt = new Point[3];
+
+            pnt[0].X = mouseX;
+            pnt[0].Y = mouseY;
+
+            pnt[1].X = mouseX - rBase;
+            pnt[1].Y = mouseY ;
+
+            pnt[2].X = mouseX;
+            pnt[2].Y = mouseY - adj;
+            g.DrawPolygon(myPen, pnt);
+        }
+
+        private void DrawRectangle(int width, int height)
+        {            
+            Pen myPen = new Pen(mainColor);
+            g.DrawRectangle(myPen, mouseX - width / 2, mouseY - height / 2, width, height);
+        }
+
+        private void DrawCircle(int radius)
+        {
+            Pen myPen = new Pen(mainColor);
+            g.DrawEllipse(myPen, mouseX - radius , mouseY - radius , radius*2, radius*2);
+        }
     }
 }
